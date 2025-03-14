b723659e5c969cd227133dff063f1c7c639da93a
choeunsung21 <eesvcho@gmail.com>
2025-03-14 16:05:31 +0900

    [ces] spring query

--- mvc/src/main/java/com/gn/mvc/controller/BoardController.java
+++ mvc/src/main/java/com/gn/mvc/controller/BoardController.java
@@ -1,16 +1,20 @@
 package com.gn.mvc.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gn.mvc.dto.BoardDto;
+import com.gn.mvc.dto.SearchDto;
+import com.gn.mvc.entity.Board;
 import com.gn.mvc.service.BoardService;
 
 import lombok.RequiredArgsConstructor;
@@ -69,6 +73,17 @@ public class BoardController {
 		
 		
 		return resultMap;
+	}
+	
+	@GetMapping("/board")
+	public String selectBoardAll(Model model, SearchDto searchDto) {
+		// 1. DB에서 목록 SELECT
+		List<Board> resultList = service.selectBoardAll(searchDto);
+		// 2. 목록 Model에 등록
+		model.addAttribute("boardList",resultList);
+		model.addAttribute("searcDto", searchDto);
+		// 3. list.html에 데이터 세팅
+		return "board/list";
 	}
 
 }
--- mvc/src/main/java/com/gn/mvc/entity/Member.java
+++ mvc/src/main/java/com/gn/mvc/entity/Member.java
@@ -1,18 +1,29 @@
 package com.gn.mvc.entity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="member")
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 	
 	@Id
@@ -28,4 +39,15 @@ public class Member {
 	
 	@Column(name="member_name")
 	private String memberName;
+	
+	@CreationTimestamp
+	@Column(updatable=false,name="reg_date")
+	private LocalDateTime regDate;
+	
+	@UpdateTimestamp
+	@Column(insertable=false,name="mod_date")
+	private LocalDateTime modDate;
+	
+	@OneToMany(mappedBy="member")
+	private List<Board> boards;
 }
