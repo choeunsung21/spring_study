28d51823651011a21e874807ad2e7e640d3892b4
choeunsung21 <eesvcho@gmail.com>
2025-03-20 15:48:28 +0900

    [ces] springboot 쿠키, 시큐리티

--- mvc/src/main/java/com/gn/mvc/service/MemberService.java
+++ mvc/src/main/java/com/gn/mvc/service/MemberService.java
@@ -1,5 +1,13 @@
 package com.gn.mvc.service;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +23,8 @@ public class MemberService {
 
 	private final MemberRepository repository;
 	private final PasswordEncoder passwordEncoder;
+	private final DataSource dataSource;
+	private final UserDetailsService userDetailsService;
 	
 	public MemberDto createMember(MemberDto dto) {
 		dto.setMember_pw(passwordEncoder.encode(dto.getMember_pw()));
@@ -23,6 +33,53 @@ public class MemberService {
 		Member result = repository.save(param);
 		
 		return new MemberDto().toDto(result);
+	}
+
+	public Member selectMemberOne(Long id) {
+		return repository.findById(id).orElse(null);
+	}
+
+	public int updateMember(MemberDto param) {
+		int result = 0;
+		try {
+			// 데이터베이스 회원 저보 수정
+			param.setMember_pw(passwordEncoder.encode(param.getMember_pw()));
+			Member updated = repository.save(param.toEntity());
+			if(updated != null) {
+				// remember-me(DB, cookie)가 있다면 무효화
+				JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+				String sql = "DELETE FROM persistent_logins WHERE username = ?";
+				jdbcTemplate.update(sql,param.getMember_id());
+				// 변경된 회원 정보 Security Context에 즉시 반영
+				UserDetails updatedUserDetails = userDetailsService.loadUserByUsername(param.getMember_id());
+				Authentication newAuth = new UsernamePasswordAuthenticationToken(updatedUserDetails,updatedUserDetails.getPassword(),updatedUserDetails.getAuthorities());
+				SecurityContextHolder.getContext().setAuthentication(newAuth);
+				result = 1;
+			}			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public int deleteMember(Long id) {
+		int result = 0;
+		try {
+			Member target = repository.findById(id).orElse(null);
+			if(target!=null) {
+				repository.deleteById(id);
+				
+				JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+				String sql = "DELETE FROM persistent_logins WHERE username = ?";
+				jdbcTemplate.update(sql,id);
+				
+				SecurityContextHolder.getContext().setAuthentication(null);
+				result = 1;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 }
