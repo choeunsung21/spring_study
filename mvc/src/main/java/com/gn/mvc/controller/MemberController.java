3c247b94ef19375cc44201746261e7ed2fbf8b0c
choeunsung21 <eesvcho@gmail.com>
2025-03-20 10:14:33 +0900

    [ces] springboot 로그인 에러 메시지

--- mvc/src/main/java/com/gn/mvc/controller/MemberController.java
+++ mvc/src/main/java/com/gn/mvc/controller/MemberController.java
@@ -4,11 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gn.mvc.dto.MemberDto;
+import com.gn.mvc.security.MemberDetailsService;
 import com.gn.mvc.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,9 +21,15 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 	
 	private final MemberService service;
+
 	
 	@GetMapping("/login")
-	public String loginView() {
+	public String loginView(
+			@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "errorMsg", required = false) String errorMsg,
+			Model model) {
+		model.addAttribute("error",error);
+		model.addAttribute("errorMsg",errorMsg);
 		return "member/login";
 	}
 	
